0f50e3bf097cd30b2a1caa1850dcef1cd64f1c47
Pranav Bansal <40292040+ipranavbansal@users.noreply.github.com>
2023-02-05 23:23:41 +0530

    bill split generataor programme

---
+++ main.py
@@ -0,0 +1,10 @@
+
+print("Welcome to the tip generator")
+
+a=float(input("input your bill amount "))
+b=float(input("Enter the tip you want to give in percentage "))
+c=int(input("No. of people to split "))
+
+bill_perperson_withtip= (a*(1+b/100))/c
+
+print("Bill per head incl tip is " + "{:.2f}".format(bill_perperson_withtip))
